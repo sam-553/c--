2a5040c5c6e183344cd99facdff2d41e8546316b
sampandit553 <sameertiwari737697@gmail.com>
2025-01-25 22:54:26 +0530

    function

--- functions/fibonnacciseries.cpp
+++ functions/fibonnacciseries.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
+int fab(int n ,int a=0,int b=1){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    for(int i=1;i<=n;i++){
+        int c=a+b;
+        a=b;
+        b=c;
+    }
+    return a;
+}
 int main(){
-    
+cout<<fab(10);
 }
