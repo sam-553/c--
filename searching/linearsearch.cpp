9b1701f4a0ed946c2eaa7dc2b17eab79e04f398c
sampandit553 <sameertiwari737697@gmail.com>
2025-01-30 21:52:15 +0530

    n

--- searching/linearsearch.cpp
+++ searching/linearsearch.cpp
@@ -14,6 +14,7 @@ int main(){
     cout<<"enter thr size of array :"<<endl;
     cin>>size;
     int arr[100];
+    
     cout << "enter the elements of arr:" << endl;
     for(int i = 0; i < size; i++) {
         cin >> arr[i];
