4a0a5228f1f01bca53168f56daa313e075846d4b
sampandit553 <sameertiwari737697@gmail.com>
2025-02-09 22:01:13 +0530

    n

--- binarytodecimal.cpp
+++ binarytodecimal.cpp
@@ -6,8 +6,7 @@ int main(){
     int temp;
     cout<<"Enter the binary number: ";
     cin>>n;
-    int sum=0;
-    int i=0;
+    int sum=0,i=0;
     while(n!=0){
         int digit = n%10;
         if(digit==1){
